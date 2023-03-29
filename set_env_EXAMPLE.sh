63b8d56d22835de4006bc0bf3b03c6e82cc70edb
Mohammed Abdullah <3bdlaa.20@gmail.com>
2023-03-29 04:00:34 +0200

    first commit

---
+++ set_env_EXAMPLE.sh
@@ -0,0 +1,14 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+# Before using, rename to set_env.sh for easier using
+# use 'source set_env.sh' to set the environment variables
+export POSTGRES_USERNAME=user
+export POSTGRES_PASSWORD=password
+export POSTGRES_PORT=5432
+export POSTGRES_HOST=postgres.cw2hsbcx2wha.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
