e1a603ed6a193c1684799346acf19738ea1a1ee5
Mohammed Abdullah <3bdlaa.20@gmail.com>
2023-04-02 01:51:05 +0200

    Update: UPdate API Host

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   //apiHost: 'http://localhost:8080/api/v0',
-  apiHost: 'a45b764df7bec4734ad520161ecb34b4-1643507577.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a45b764df7bec4734ad520161ecb34b4-1643507577.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
